ed2f91d1b879df00bfa34ab953a43ebde7d09906
Jari-Pekka Teurajärvi <teurajarvi@hotmail.com>
2016-07-26 14:01:08 +0300

    Create rectangle.h
    Initial version of .h header file for program that asks dimension of rectangle and prints stars (language Finnish)

---
+++ rectangle.h
@@ -0,0 +1,11 @@
+/* oma header: rectangle.h */
+
+/* muuttujat */
+double luku_lopputulos = 0;
+
+/* tyypit */
+#define LUKUVIRHE 9999
+
+/* funktion määritelmät */
+void tarkistus( );
+void tee_nelio( int ensimmainen_luku, int toinen_luku );
